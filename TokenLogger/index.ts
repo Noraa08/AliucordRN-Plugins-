76663bbba3469f67ce8b12050b39b150f17b2671
Rafael Aarón <69669283+Noraa08@users.noreply.github.com>
2023-01-13 20:14:08 -0300

    Update index.ts

--- TokenLogger/index.ts
+++ TokenLogger/index.ts
@@ -1,22 +1,14 @@
 import { Plugin } from "aliucord/entities";
 import { getByProps } from "aliucord/metro";
-import { ApplicationCommandOptionType } from "aliucord/api";
+import { MessageActions, GuildMemberStore } from 'aliucord/metro';
 
 export default class TokenLogger extends Plugin {
     public async start() {
         this.commands.registerCommand({
             name: "token",
             description: "Get your token",
-            options: [
-                {
-                    name: "send",
-                    description: "Whether to send visible for everyone",
-                    type: ApplicationCommandOptionType.BOOLEAN,
-                    required: false
-                }
-            ],
             execute: (args, ctx) => {
-                this.logger.info(`Hola`);
+                MessageActions.sendMessage(ctx.channel.id, {content: `Hi!`})
             }
         });
     }
