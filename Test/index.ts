269464491b1a74ead96a3f27d47dae4068004375
Rafael Aarón <69669283+Noraa08@users.noreply.github.com>
2023-01-14 22:31:14 -0300

    Update and rename TokenLogger/index.ts to Test/index.ts

---
+++ Test/index.ts
@@ -0,0 +1,16 @@
+import { Plugin } from "aliucord/entities"; 
+import { MessageActions, GuildMemberStore } from 'aliucord/metro'; 
+  
+export default class Test extends Plugin { 
+    public async start() { 
+  
+        this.commands.registerCommand({ 
+            name: "tesy", 
+            description: "test plugin", 
+            options: [], 
+            execute(_, ctx) { 
+                MessageActions.sendMessage(ctx.channel.id, {content: "Hello! This is working"}) 
+            } 
+        }); 
+    } 
+ }
